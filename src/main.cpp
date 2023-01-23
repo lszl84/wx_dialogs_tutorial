5b49e699ced28ce4cf66da46bdf204a9318e16be
Lukasz <lukasz.sromek@gmail.com>
2023-01-23 13:32:12 +0100

    basic wxMessageBox

--- src/main.cpp
+++ src/main.cpp
@@ -24,4 +24,8 @@ wxIMPLEMENT_APP(MyApp);
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    auto button = new wxButton(this, wxID_ANY, "Show Dialog");
+
+    button->Bind(wxEVT_BUTTON, [this](wxCommandEvent &event)
+                 { wxMessageBox("Hello World", "Message", wxOK | wxICON_INFORMATION); });
 }
