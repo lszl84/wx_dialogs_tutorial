ac8eabb73c8d3ec0c6bd5b0fa387d8d0f5cf241a
Lukasz <lukasz.sromek@gmail.com>
2023-01-23 13:20:49 +0100

    basic app

---
+++ src/main.cpp
@@ -0,0 +1,27 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp
+{
+public:
+    virtual bool OnInit();
+};
+
+class MyFrame : public wxFrame
+{
+public:
+    MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+};
+
+bool MyApp::OnInit()
+{
+    MyFrame *frame = new MyFrame("Hello World", wxDefaultPosition, wxDefaultSize);
+    frame->Show(true);
+    return true;
+}
+
+wxIMPLEMENT_APP(MyApp);
+
+MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
+    : wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+}
