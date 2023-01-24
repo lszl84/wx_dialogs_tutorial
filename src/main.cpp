41e31aa67fb6e9200195b979bb335bc52f0282b8
Lukasz <lukasz.sromek@gmail.com>
2023-01-24 23:15:58 +0100

    progress dialog for bubble sort

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <wx/wx.h>
 #include <wx/progdlg.h>
 
+#include <vector>
+
 class MyApp : public wxApp
 {
 public:
@@ -11,6 +13,9 @@ class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
+
+private:
+    std::vector<int> data;
 };
 
 bool MyApp::OnInit()
@@ -22,9 +27,34 @@ bool MyApp::OnInit()
 
 wxIMPLEMENT_APP(MyApp);
 
+void BubbleSort(std::vector<int> &data, std::function<bool(int)> shouldQuit)
+{
+    for (int i = 0; i < data.size(); i++)
+    {
+        if (shouldQuit(i * 100 / data.size()))
+        {
+            return;
+        }
+
+        for (int j = 0; j < data.size() - i - 1; j++)
+        {
+
+            if (data[j] > data[j + 1])
+            {
+                std::swap(data[j], data[j + 1]);
+            }
+        }
+    }
+}
+
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    for (int i = 0; i < 30000; i++)
+    {
+        data.push_back(rand());
+    }
+
     auto button = new wxButton(this, wxID_ANY, "Show Dialog");
     auto textView = new wxStaticText(this, wxID_ANY, "Click the button to show the dialog");
 
@@ -35,17 +65,17 @@ MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
 
     this->SetSizer(sizer);
 
+    textView->SetLabel("First Item: " + std::to_string(data[0]));
+
     button->Bind(wxEVT_BUTTON, [this, textView](wxCommandEvent &event)
                  {
                      wxProgressDialog dialog("Progress Dialog", "Doing work", 100, this, wxPD_AUTO_HIDE | wxPD_APP_MODAL | wxPD_CAN_ABORT | wxPD_ELAPSED_TIME | wxPD_ESTIMATED_TIME | wxPD_REMAINING_TIME);
 
-                     const int cnt = 100000;
-                     for (int i = 0; i < cnt; i++)
-                     {
-                         if (!dialog.Update(i * 100 / cnt))
-                         {
-                             textView->SetLabel("Cancelled");
-                             break;
-                         }
-                     } });
+                     BubbleSort(data, [&dialog](int progress)
+                                { return !dialog.Update(progress); });
+
+                     dialog.Update(100);
+
+                     textView->SetLabel("First Item: " + std::to_string(data[0]));
+                 });
 }
