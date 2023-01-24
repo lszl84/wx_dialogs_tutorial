42bef8863a7877286f61193fcdba162a4acc75f3
Lukasz <lukasz.sromek@gmail.com>
2023-01-24 23:02:20 +0100

    bitmap manipulation code moved to separate file

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,4 @@
 #include <wx/wx.h>
-#include <wx/colordlg.h>
-#include <wx/fontdlg.h>
-
-#include "brightnessdialog.h"
-#include "brightnesschangeevent.h"
-
-#include "bufferedbitmap.h"
 
 class MyApp : public wxApp
 {
@@ -17,25 +10,10 @@ class MyFrame : public wxFrame
 {
 public:
     MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size);
-
-private:
-    BufferedBitmap *staticBitmap;
-    wxStaticText *textView;
-
-    wxImage image;
-
-    void OnOpenImage(wxCommandEvent &event);
-    void OnChangeColor(wxCommandEvent &event);
-    void OnChangeFont(wxCommandEvent &event);
-    void OnChangeBrightness(wxCommandEvent &event);
-
-    void UpdateBitmapImage(const wxImage &image);
 };
 
 bool MyApp::OnInit()
 {
-    wxInitAllImageHandlers();
-
     MyFrame *frame = new MyFrame("Hello World", wxDefaultPosition, wxDefaultSize);
     frame->Show(true);
     return true;
@@ -46,121 +24,17 @@ wxIMPLEMENT_APP(MyApp);
 MyFrame::MyFrame(const wxString &title, const wxPoint &pos, const wxSize &size)
     : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+    auto button = new wxButton(this, wxID_ANY, "Show Dialog");
+    auto textView = new wxStaticText(this, wxID_ANY, "Click the button to show the dialog");
+
     auto sizer = new wxBoxSizer(wxVERTICAL);
 
-    textView = new wxStaticText(this, wxID_ANY, "Here's your image:");
-    staticBitmap = new BufferedBitmap(this, wxID_ANY, wxBitmap(wxSize(1, 1)), wxDefaultPosition, FromDIP(wxSize(500, 200)));
+    sizer->Add(button, 0, wxALL, FromDIP(10));
+    sizer->Add(textView, 0, wxALL, FromDIP(10));
 
-    auto imageButton = new wxButton(this, wxID_ANY, "Load Image...");
-    auto colorButton = new wxButton(this, wxID_ANY, "Change Color...");
-    auto fontButton = new wxButton(this, wxID_ANY, "Change Font...");
-    auto brightnessButton = new wxButton(this, wxID_ANY, "Change Brightness...");
+    this->SetSizer(sizer);
 
-    imageButton->Bind(wxEVT_BUTTON, &MyFrame::OnOpenImage, this);
-    colorButton->Bind(wxEVT_BUTTON, &MyFrame::OnChangeColor, this);
-    fontButton->Bind(wxEVT_BUTTON, &MyFrame::OnChangeFont, this);
-    brightnessButton->Bind(wxEVT_BUTTON, &MyFrame::OnChangeBrightness, this);
+    button->Bind(wxEVT_BUTTON, [this, textView](wxCommandEvent &event) {
 
-    auto buttonSizer = new wxBoxSizer(wxHORIZONTAL);
-    buttonSizer->Add(imageButton, 0, wxLEFT, FromDIP(5));
-    buttonSizer->Add(colorButton, 0, wxLEFT, FromDIP(5));
-    buttonSizer->Add(fontButton, 0, wxLEFT, FromDIP(5));
-    buttonSizer->Add(brightnessButton, 0, wxLEFT, FromDIP(5));
-
-    sizer->Add(textView, 0, wxALIGN_CENTER | wxLEFT | wxRIGHT | wxTOP, FromDIP(10));
-    sizer->Add(staticBitmap, 1, wxEXPAND | wxALL, FromDIP(10));
-    sizer->Add(buttonSizer, 0, wxALIGN_RIGHT | wxLEFT | wxRIGHT | wxBOTTOM, FromDIP(10));
-
-    this->SetSizerAndFit(sizer);
-}
-
-void MyFrame::OnOpenImage(wxCommandEvent &event)
-{
-    wxFileDialog dialog(this, "Open Image File", "", "", "Image Files (*.png;*.jpg;*.jpeg;*.bmp)|*.png;*.jpg;*.jpeg;*.bmp", wxFD_OPEN | wxFD_FILE_MUST_EXIST);
-
-    if (dialog.ShowModal() == wxID_CANCEL)
-    {
-        return;
-    }
-
-    if (!image.LoadFile(dialog.GetPath()))
-    {
-        wxMessageBox("Failed to load image file", "Error", wxOK | wxICON_ERROR);
-        return;
-    }
-
-    UpdateBitmapImage(image);
-}
-
-void MyFrame::OnChangeColor(wxCommandEvent &event)
-{
-    wxColourData data;
-    data.SetColour(this->GetBackgroundColour());
-    wxColourDialog dialog(this, &data);
-
-    if (dialog.ShowModal() == wxID_OK)
-    {
-        auto backgroundColor = dialog.GetColourData().GetColour();
-        this->SetBackgroundColour(backgroundColor);
-        staticBitmap->SetBackgroundColour(backgroundColor);
-        this->Refresh();
-    }
-}
-
-void MyFrame::OnChangeFont(wxCommandEvent &event)
-{
-    wxFontData data;
-    data.SetInitialFont(textView->GetFont());
-    wxFontDialog dialog(this, data);
-
-    if (dialog.ShowModal() == wxID_OK)
-    {
-        auto font = dialog.GetFontData().GetChosenFont();
-        textView->SetFont(font);
-        this->Fit();
-        this->Layout();
-    }
-}
-
-void MyFrame::OnChangeBrightness(wxCommandEvent &event)
-{
-    if (!image.IsOk())
-    {
-        wxMessageBox("Please load an image first", "Error", wxOK | wxICON_ERROR);
-    }
-    else
-    {
-        BrightnessDialog dialog(this);
-
-        dialog.Bind(wxEVT_BRIGHTNESS_CHANGED, [&](BrightnessChangeEvent &event)
-                    {
-                        double adj = event.GetBrightnessAdjustment();
-
-                        auto previewImage = image.Copy();
-                        previewImage.ChangeBrightness(adj);
-                        UpdateBitmapImage(previewImage);
-
-                        previewImage.Destroy(); });
-
-        dialog.Bind(EVT_PREVIEW_CHECKBOX_CHANGED, [&](wxCommandEvent &event)
-                    {
-                        if (event.GetInt() == 0)
-                        {
-                            UpdateBitmapImage(image);
-                        } });
-
-        if (dialog.ShowModal() == wxID_OK)
-        {
-            double adj = dialog.GetBrightnessAdjustment();
-            image.ChangeBrightness(adj);
-        }
-
-        UpdateBitmapImage(image);
-    }
-}
-
-void MyFrame::UpdateBitmapImage(const wxImage &image)
-{
-    staticBitmap->SetBitmap(wxBitmap(image));
-    this->Layout();
+        });
 }
